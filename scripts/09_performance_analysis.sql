4c744005f14ae252acd724dfde125576b703cf0d
kareemshaltout10 <kareemshaltout678@gmail.com>
2026-08-01 23:02:50 +0300

    Create 09_performance_analysis.sql

---
+++ scripts/09_performance_analysis.sql
@@ -0,0 +1,16 @@
+-- Does the company deliver orders early, on time, or late?
+
+select
+count(*) as orders,
+delivery_status 
+from(
+	select
+	case 
+		when DATEDIFF(day, order_delivered_customer_date, order_estimated_delivery_date) > 0 then 'Delivered Early'
+		when DATEDIFF(day, order_delivered_customer_date, order_estimated_delivery_date) = 0 then 'Delivered On Time'
+		else 'Delivered Late'
+	end delivery_status
+	from orders
+	where order_delivered_customer_date is not null 
+)t
+group by delivery_status	
