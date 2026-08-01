e465444f817c4770c62cc1c309e1aac6a8936065
kareemshaltout10 <kareemshaltout678@gmail.com>
2026-08-01 23:02:41 +0300

    Create 07_change_over_time_analysis.sql

---
+++ scripts/07_change_over_time_analysis.sql
@@ -0,0 +1,40 @@
+-- total sales by year 
+
+select 
+cast(datetrunc(month,o.order_delivered_customer_date) as date) date_month ,
+sum(i.price) as total_sales 
+from orders o
+join order_items i
+on o.order_id = i.order_id
+where o.order_status = 'delivered'
+group by datetrunc(month,o.order_delivered_customer_date)  
+order by datetrunc(month,o.order_delivered_customer_date) 
+
+-- How has the company's revenue changed month over month?
+
+with total_sales_month as
+(
+	select 
+	cast(datetrunc(month,o.order_purchase_timestamp) as date) date_month,
+	sum(i.price) as total_revenue
+	from orders o
+	inner join order_items i
+	on i.order_id = o.order_id
+	where o.order_status = 'delivered'
+	group by datetrunc(month,o.order_purchase_timestamp)   
+),
+previous_month as
+(
+	select 
+	*,
+	lag(total_revenue) over(order by date_month ) as previous_month_revenue
+	from total_sales_month
+)
+
+select 
+*,
+total_revenue - previous_month_revenue as revenue_difference ,
+round((total_revenue - previous_month_revenue) * 100.0 / nullif(previous_month_revenue,0) ,2) as growth_percentage
+from previous_month
+
+
