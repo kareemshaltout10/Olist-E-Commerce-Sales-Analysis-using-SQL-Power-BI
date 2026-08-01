3f4be36a057517e8fb6332a6685eaed3ccc4c460
kareemshaltout10 <kareemshaltout678@gmail.com>
2026-08-01 23:02:21 +0300

    Create 01_database_exploration.sql

---
+++ scripts/01_database_exploration.sql
@@ -0,0 +1,46 @@
+-- How many records are in each table?
+
+select 'customers' as table_name, count(*) as total_records
+from customers
+
+union all
+
+select 'orders', count(*)
+from orders
+
+union all
+
+select 'order_items', count(*)
+from order_items
+
+union all
+
+select 'products', count(*)
+from products
+
+union all
+
+select 'sellers', count(*)
+from sellers
+
+union all
+
+select 'payments', count(*)
+from payments
+
+union all
+
+select 'reviews', count(*)
+from reviews
+
+union all
+
+select 'geolocation', count(*)
+from geolocation
+
+union all
+
+select 'category_translation', count(*)
+from category_translation
+
+order by total_records desc;
